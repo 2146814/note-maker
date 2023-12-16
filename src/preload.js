cecd81f3ac6d02737e3e4fad26ea476fc82ba674
JUSTIN PAGÈS <spyrojustin@gmail.com>
2023-12-16 13:38:47 -0500

    3 - Affichage et écriture des données JSON

--- src/preload.js
+++ src/preload.js
@@ -4,17 +4,19 @@
 //Importation de module Node.js
 const fs = require("fs");
 
-window.addEventListener("DOMContentLoaded", () => {
-  //Ressources
-  const notes = readNotesFromJson();
+//Ressources
+let notes = readNotesFromJson();
 
+window.addEventListener("DOMContentLoaded", () => {
   //Widgets
   const notesContainer = document.getElementById("notes-container");
 
   const form = document.getElementById("form-newNote");
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log("Je veux ajouter une note !");
+    const newNote = { title: newNoteTitle.value, content: newNoteContent.value };
+    writeNoteToJson(newNote);
+    AddNoteToPage(notesContainer, newNote);
   });
 
   const newNoteTitle = document.getElementById("newNoteTitle");
@@ -23,13 +25,18 @@ window.addEventListener("DOMContentLoaded", () => {
     today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
 
   const newNoteContent = document.getElementById("newNoteContent");
+
+  //Initialisation de la page
+  notes.forEach((note) => {
+    AddNoteToPage(notesContainer, note);
+  });
 });
 
 //Fonctions
 function readNotesFromJson() {
   var notes = [];
 
-  // Utiliser la fonction readFileSync pour 
+  // Utiliser la fonction readFileSync pour
   //lire le fichier de manière synchrone
   try {
     const jsonData = JSON.parse(
@@ -37,10 +44,45 @@ function readNotesFromJson() {
     );
 
     notes = jsonData;
-    console.log(notes);
   } catch (error) {
     console.error("Error reading or parsing JSON file:", error.message);
   }
 
   return notes;
+}
+
+function writeNoteToJson(note) {
+  const jsonNotes = JSON.stringify(notes.push(note), null, 2);
+
+  // Écrire dans le fichier
+  fs.writeFile(__dirname + "/data.json", jsonNotes, "utf-8", (err) => {
+    if (err) {
+      console.error("Erreur lors de l'écriture du fichier JSON :", err);
+    }
+  });
+}
+
+function AddNoteToPage(container, note) {
+  var noteContainer = document.createElement("div");
+
+  var noteTitle = document.createElement("strong");
+  noteTitle.innerHTML = note.title;
+
+  var noteContent = document.createElement("p");
+  noteContent.innerHTML = note.content.replace(/\n/g, "<br>");
+
+  var deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Supprimer";
+
+  // Attacher un gestionnaire d'événements pour la suppression
+  deleteButton.addEventListener("click", function () {
+    console.log("Je ne veux plus voir cette note !");
+  });
+
+  noteContainer.appendChild(noteTitle);
+  noteContainer.appendChild(noteContent);
+  noteContainer.appendChild(deleteButton);
+  noteContainer.classList.add("note"); //Classe CSS
+
+  container.appendChild(noteContainer);
 }
